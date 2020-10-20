1b2e2db31d83cf21610dde0f475203500806953f
Temirlan <temirlan.balguzhin@bk.ru>
2020-10-20 11:08:29 +0600

    feat: implement function for decode

--- src/index.js
+++ src/index.js
@@ -37,10 +37,52 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+// const expression = '00000010111110101010111111111111101010101111111110001010111010101010101111111111';
+
+// разделить массивами
+
 function decode(expr) {
-    // write your solution here
+  const grouppedArr = [];
+
+  function groupArr(arr, n) {
+    let someArr = [];
+    
+    for (let i = 0; i < n; i++) {
+        someArr.push(arr[i]);
+    }
+    
+    grouppedArr.push(someArr);
+    arr.splice(0, n)
+    
+    if (arr.length > 0) {
+      groupArr(arr, n);
+    }
+    
+    return grouppedArr;
+  }
+
+  const arr = expr.split('');
+  const strArr = groupArr(arr, 10).map(arr => arr.join(''));
+  const cleanArr = strArr.map(str => str.replace(/\*{10}/g, ' ').replace(/00/g, ''));
+  const morseArr = cleanArr.map(str => str.replace(/10/g, '.').replace(/11/g, '-'));
+  const morseTable = Object.keys(MORSE_TABLE);
+  const letterArr = [];
+  
+  for (let i = 0; i < morseArr.length; i++) {
+    if (morseArr[i] === ' ') {
+      letterArr.push(' ');
+    }
+     
+    for (let k = 0; k < morseTable.length; k++) {
+      if (morseArr[i] === morseTable[k]) {
+        letterArr.push(MORSE_TABLE[morseTable[k]]);
+      }    
+    }
+  } 
+  
+  return letterArr.reduce((a, b) => a + b, '');
 }
 
 module.exports = {
-    decode
-}
+  decode
+};
